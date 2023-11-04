91081b8a8ca735efbc80b379961363911714ebb4
xxyzz <gitpull@protonmail.com>
2023-11-04 15:18:31 +0800

    Add `code_of_name` parameter to `name_to_code()`

--- src/mediawiki_langcodes/convert.py
+++ src/mediawiki_langcodes/convert.py
@@ -44,15 +44,21 @@ def code_to_name(lang_code: str, in_language: str = "") -> str:
     return lang_name
 
 
-def name_to_code(lang_name: str) -> str:
+def name_to_code(lang_name: str, code_of_name: str = "") -> str:
     """
+    Pass the language code of the language name to limit the search scope and reduce
+    ambiguity.
     Return empty string if `lang_name` doesn't exists.
     """
     conn = sqlite3.connect(DB_PATH)
     lang_code = ""
-    for (result_code,) in conn.execute(
-        "SELECT lang_code FROM langcodes WHERE lang_name = ? LIMIT 1", (lang_name,)
-    ):
+    search_sql = "SELECT DISTINCT lang_code FROM langcodes WHERE lang_name = ?"
+    search_values = (lang_name,)
+    if code_of_name != "":
+        search_sql += " AND code_of_name = ?"
+        search_values = (lang_name, code_of_name)
+    search_sql += " ORDER BY length(lang_code) LIMIT 1"
+    for (result_code,) in conn.execute(search_sql, search_values):
         lang_code = result_code
 
     conn.close()
