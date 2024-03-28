79ba5b8528ab0b9b6e122a13f44870f597332f98
xxyzz <gitpull@protonmail.com>
2024-03-28 14:50:57 +0800

    Add language names not included in page "Hilfe:Sprachkürzel"

--- build_script/wiktionary_de.py
+++ build_script/wiktionary_de.py
@@ -26,4 +26,48 @@ def add_de_wiktionary_languages(conn: Connection) -> None:
                 elif index in (0, 2):
                     lang_name_str = "".join(td_tag.itertext()).strip()
                     for lang_name in lang_name_str.split("/"):
-                        insert_data(conn, lang_code, lang_name, WIKTIONARY_LANG_CODE)
+                        insert_data(
+                            conn, lang_code, lang_name.strip(), WIKTIONARY_LANG_CODE
+                        )
+
+    for lang_code, lang_name in EXTRA_LANG_NAMES:
+        insert_data(conn, lang_code, lang_name, WIKTIONARY_LANG_CODE)
+
+
+EXTRA_LANG_NAMES = (
+    ("mul", "International"),
+    ("pnb", "West-Pandschabi"),
+    ("nhn", "Zentral-Nahuatl"),
+    ("spx", "Südpikenisch"),
+    ("de", "Frühneuhochdeutsch"),  # Early New High German, doesn't have ISO code
+    ("xum", "Umbrisch"),
+    ("xvo", "Volskisch"),
+    ("ims", "Marsisch"),  # https://de.wikipedia.org/wiki/Marsische_Sprache
+    ("xvs", "Vestinisch"),
+    ("osc", "Oskisch"),
+    ("jam", "Jamaika-Kreolisch"),  # https://en.wikipedia.org/wiki/Jamaican_Patois
+    ("nhw", "Huastekisches West-Nahuatl"),  # Huasteca Nahuatl
+    ("ote", "Mezquital-Otomi"),  # https://en.wikipedia.org/wiki/Northwestern_Otomi
+    ("nhe", "Huastekisches Ost-Nahuatl"),  # Huastec Eastern Nahuatl
+    ("nci", "Klassisches Nahuatl‎"),  # https://en.wikipedia.org/wiki/Classical_Nahuatl
+    ("nhv", "Temascaltepec-Nahuatl"),
+    ("nhg", "Tetelcingo-Nahuatl"),
+    ("xae", "Äquisch"),
+    ("nlv", "Orizaba-Nahuatl"),
+    ("cs", "Alttschechisch"),
+    ("qu", "Argentinisches Quechua"),
+    ("hus", "Huastekisch"),
+    ("grc", "Mittelgriechisch"),
+    ("tkl", "Tokelauisch"),
+    ("ncx", "Zentrales Puebla-Nahuatl"),
+    ("ngu", "Guerrero-Nahuatl"),
+    ("zai", "Isthmus-Zapotekisch"),
+    ("odt", "Altniederländisch"),
+    ("mrv", "Mangarevanisch"),
+    ("umc", "Marrukinisch"),
+    ("kj", "Oshivambo"),
+    ("ota", "Osmanisches Türkisch"),
+    ("pcd", "Pikardisch"),
+    ("pkp", "Pukapuka"),
+    ("mpm", "Yosondúa-Mixtekisch"),
+)
